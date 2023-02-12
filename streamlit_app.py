dca670740d2b309b61b247fe445f9fc219c29d99
Simon Woo <simon@simoncomputing.com>
2023-02-11 23:34:52 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
